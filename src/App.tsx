e5f48d8c0cb31ac01468463a3b8e47734f21af53
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 17:16:27 +0000

    starting page markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from "react";
 import Theme from "./theme/Theme";
 import { ThemeProvider } from "styled-components";
+import PositionSearchbar from "./components/PositionSearchbar";
 
 const App: FC = () => {
   return (
     <div className="page__wrapper">
       <ThemeProvider theme={Theme}>
+        <header className="page__header">
+          <h6 className="header__logo">
+            <span className="strong__text">Github</span>
+            Jobs
+          </h6>
+          <PositionSearchbar />
+        </header>
         <h1>hello mate!</h1>
       </ThemeProvider>
     </div>
