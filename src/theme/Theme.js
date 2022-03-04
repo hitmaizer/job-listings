1819b1865f8badbd33c6c18e47c045ca99b15502
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 16:48:36 +0000

    imported theme file

---
+++ src/theme/Theme.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+    colors: {
+        primaryBlue: "#1E86FF",
+        darkPurple: "#282538",
+        white: "#FFFFFF",
+        lightGray: "#B9BDCF",
+        blueGray: "#334680",
+        offGray: "#B7BCCE",
+        footerGray: "#B9BDCF",
+         
+
+    },
+
+    dropShadow: {
+        dropShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)"
+    }
+
+}
