cddfd22e8be6fcb7b9eec61af3489b42df029c57
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 16:47:39 +0000

    imported styledcard file

---
+++ src/elements/StyledCard.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    background-color: ${(props) => props.theme.colors.white};
+    border-radius: 4px;
+    min-height: 114px;
+`
