fe989b166503a305e48a89caa1ea3d7634760536
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 17:46:18 +0000

    added offwhite bg color

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,20 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export default {
-    colors: {
-        primaryBlue: "#1E86FF",
-        darkPurple: "#282538",
-        white: "#FFFFFF",
-        lightGray: "#B9BDCF",
-        blueGray: "#334680",
-        offGray: "#B7BCCE",
-        footerGray: "#B9BDCF",
-         
+  colors: {
+    primaryBlue: "#1E86FF",
+    darkPurple: "#282538",
+    white: "#FFFFFF",
+    lightGray: "#B9BDCF",
+    blueGray: "#334680",
+    offGray: "#B7BCCE",
+    footerGray: "#B9BDCF",
+    offWhite: "#F6F7FB",
+  },
 
-    },
-
-    dropShadow: {
-        dropShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)"
-    }
-
-}
+  dropShadow: {
+    dropShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)",
+  },
+};
