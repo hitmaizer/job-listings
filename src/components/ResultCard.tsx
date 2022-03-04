2c0fe91f1a85d90133c4ce64e31ed12fba523582
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 18:11:45 +0000

    result card markup adjusts

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -6,18 +6,18 @@ const ResultCard: FC = () => {
   return (
     <StyledCard className="card__wrapper flex-row">
       <img src="images/placeholder.png" alt="" className="card__img" />
-      <div className="card__info">
+      <div className="card__info flex-col">
         <h6 className="info__company">Kasisto</h6>
         <h1 className="info__title">Front-End Software Engineer</h1>
         <h5 className="info__badge">Full time</h5>
       </div>
       <div className="card__details flex-row">
         <div className="detail__item flex-row">
-          <Globe size="24px" />
+          <Globe size="16px" />
           <p className="item__text">New York</p>
         </div>
         <div className="detail__item flex-row">
-          <Clock size="24px" />
+          <Clock size="16px" />
           <p className="item__text">5 days ago</p>
         </div>
       </div>
