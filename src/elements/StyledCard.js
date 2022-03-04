523059c4edea4884fa29b0d31bc96d53b476186f
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 17:46:31 +0000

    basic card styling

--- src/elements/StyledCard.js
+++ src/elements/StyledCard.js
@@ -1,7 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export default styled.div`
-    background-color: ${(props) => props.theme.colors.white};
-    border-radius: 4px;
-    min-height: 114px;
-`
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 4px;
+  min-height: 114px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
+  padding: 12px;
+`;
