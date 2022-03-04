3f8de734f0d97bd4372b2f1bc70f3d945486d587
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 17:16:41 +0000

    starting position searchbar markup

--- src/components/PositionSearchbar.tsx
+++ src/components/PositionSearchbar.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import { BriefcaseFill } from "styled-icons/bootstrap";
 
 const PositionSearchbar: FC = () => {
-  return <div>PositionSearchbar</div>;
+  return (
+    <div className="section__wrapper">
+      <div className="input__container">
+        <BriefcaseFill size="24px" />
+        <input type="text" className="search__input" />
+        <button className="search__btn">Search</button>
+      </div>
+    </div>
+  );
 };
 
 export default PositionSearchbar;
