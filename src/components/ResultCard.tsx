4f6295b9928ac7900b5fe90802fbbdbd6aef5331
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 17:46:51 +0000

    result card markup

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,7 +1,28 @@
 import React, { FC } from "react";
+import { Clock, Globe } from "styled-icons/entypo";
+import StyledCard from "../elements/StyledCard";
 
 const ResultCard: FC = () => {
-  return <div>ResultCard</div>;
+  return (
+    <StyledCard className="card__wrapper flex-row">
+      <img src="images/placeholder.png" alt="" className="card__img" />
+      <div className="card__info">
+        <h6 className="info__company">Kasisto</h6>
+        <h1 className="info__title">Front-End Software Engineer</h1>
+        <h5 className="info__badge">Full time</h5>
+      </div>
+      <div className="card__details flex-row">
+        <div className="detail__item flex-row">
+          <Globe size="24px" />
+          <p className="item__text"></p>
+        </div>
+        <div className="detail__item flex-row">
+          <Clock size="24px" />
+          <p className="item__text"></p>
+        </div>
+      </div>
+    </StyledCard>
+  );
 };
 
 export default ResultCard;
