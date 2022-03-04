2a55b7cf39ce10a65b0b1421198d2c1c8d08fe03
hitmaizer <jose.sousalves@gmail.com>
2022-03-04 19:06:30 +0000

    flex-col on wrapper + class names for styling

--- src/components/PositionSearchbar.tsx
+++ src/components/PositionSearchbar.tsx
@@ -3,10 +3,14 @@ import { BriefcaseFill } from "styled-icons/bootstrap";
 
 const PositionSearchbar: FC = () => {
   return (
-    <div className="section__wrapper position">
+    <div className="section__wrapper position flex-col">
       <div className="input__container">
-        <BriefcaseFill size="24px" />
-        <input type="text" className="search__input" />
+        <BriefcaseFill size="16px" className="input__icon" />
+        <input
+          type="text"
+          className="search__input"
+          placeholder="Title, companies, expertise or benefits"
+        />
         <button className="search__btn">Search</button>
       </div>
     </div>
