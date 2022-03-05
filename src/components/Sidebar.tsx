9840ed06495227c4fa791de720ebe0e1331f29d7
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 11:19:14 +0000

    sidebar markup

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { KeyboardBackspace } from "styled-icons/material";
+
+type Props = {};
+
+function Sidebar({}: Props) {
+  return (
+    <div className="sidebar__wrapper flex-col">
+      <div className="sidebar__back flex-row">
+        <KeyboardBackspace size="16px" className="back__icon" />
+        <p className="back__text">Back to search</p>
+      </div>
+      <h6 className="sidebar__title">How to apply</h6>
+      <p className="sidebar__text">
+        Please email a copy of your resume and online portfolio to
+        <a href="mailto:hello@example.com" className="sidebar__emails">
+          {" "}
+          wes@kasisto.com
+        </a>{" "}
+        &amp; CC{" "}
+        <a href="mailto:hello@example.com" className="sidebar__emails">
+          eric@kasisto.com
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Sidebar;
