3044b54b213d3f9a789f231d4ca33cf362ebe1a0
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 15:59:39 +0000

    created a interface type for data obj

---
+++ src/interfaces/DataInterface.ts
@@ -0,0 +1,12 @@
+export interface DataObj {
+  url: string;
+  title: string;
+  company_name: string;
+  company_logo: string;
+  category: string;
+  tags: string[];
+  job_type: string;
+  candidate_location: string;
+  description: string;
+  publication_date: string;
+}
