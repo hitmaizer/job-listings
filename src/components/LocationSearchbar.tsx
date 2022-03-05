4d2fbeb2de8aaed6ecae76b78878c9524594d327
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 10:19:53 +0000

    jsx markup for location sidebar component

--- src/components/LocationSearchbar.tsx
+++ src/components/LocationSearchbar.tsx
@@ -1,7 +1,44 @@
 import React, { FC } from "react";
+import { Globe } from "styled-icons/entypo";
 
 const LocationSearchbar: FC = () => {
-  return <div>LocationSearchbar</div>;
+  return (
+    <div className="section__wrapper flex-col">
+      <div className="sidebar__checkbox flex-row">
+        <input type="checkbox" className="fulltime__checkbox" />
+        <p className="fulltime__text">Full time</p>
+      </div>
+      <h6 className="sidebar__title">Location</h6>
+      <div className="input__container--location">
+        <Globe size="12px" />
+        <input type="text" className="location__search" />
+      </div>
+      <div className="radio__item flex-row">
+        <input type="radio" className="location__radio" value="London" />
+        <label htmlFor="London" className="radio__label">
+          London
+        </label>
+      </div>
+      <div className="radio__item flex-row">
+        <input type="radio" className="location__radio" value="Amesterdam" />
+        <label htmlFor="Amesterdam" className="radio__label">
+          Amesterdam
+        </label>
+      </div>
+      <div className="radio__item flex-row">
+        <input type="radio" className="location__radio" value="New York" />
+        <label htmlFor="New York" className="radio__label">
+          New York
+        </label>
+      </div>
+      <div className="radio__item flex-row">
+        <input type="radio" className="location__radio" value="Berlin" />
+        <label htmlFor="Berlin" className="radio__label">
+          Berlin
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default LocationSearchbar;
