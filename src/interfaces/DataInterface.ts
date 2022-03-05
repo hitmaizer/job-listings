031f6c059a7d97cb60869c4c8d97557573384dc6
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 19:15:05 +0000

    interface fix

--- src/interfaces/DataInterface.ts
+++ src/interfaces/DataInterface.ts
@@ -1,12 +1,17 @@
 export interface DataObj {
-  url: string;
-  title: string;
-  company_name: string;
-  company_logo: string;
-  category: string;
-  tags: string[];
-  job_type: string;
-  candidate_location: string;
-  description: string;
-  publication_date: string;
+  posts: {
+    id?: number;
+    url: string;
+    title: string;
+    company_name: string;
+    company_logo: string;
+    category: string;
+    tags: string[];
+    job_type: string;
+    publication_date: string;
+    candidate_required_location: string;
+    salary?: string;
+    description: string;
+    company_logo_url?: string;
+  }[];
 }
