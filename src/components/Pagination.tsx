a2b24695afab9c4fb98a52d94362fea49057d1c1
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 10:56:45 +0000

    pagination jsx markup

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { NavigateBefore, NavigateNext } from "styled-icons/material";
+import StyledPagination from "../elements/StyledPagination";
 
 type Props = {};
 
 function Pagination({}: Props) {
-  return <div>Pagination</div>;
+  return (
+    <div className="pagination__wrapper flex-row">
+      <StyledPagination>
+        <NavigateBefore size="16px" />
+      </StyledPagination>
+      <StyledPagination>
+        <NavigateNext size="16px" />
+      </StyledPagination>
+    </div>
+  );
 }
 
 export default Pagination;
