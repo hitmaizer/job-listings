111ebc93d48a931cb5db8ecadbcb7c0c34a46ec1
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 19:14:39 +0000

    data following dataObj interface

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,21 @@ import PositionSearchbar from "./components/PositionSearchbar";
 import LocationSearchbar from "./components/LocationSearchbar";
 import Results from "./components/Results";
 import Article from "./components/Article";
+import Data from "./Data.json";
+import { DataObj } from "./interfaces/DataInterface";
 
 const App: FC = () => {
   const [searching, setSearching] = React.useState(true);
+  const [displayData, setDisplayData] = React.useState<DataObj[]>([]);
+  const [allData, setAllData] = React.useState<DataObj["posts"]>(Data);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [postsPerPage, setPostsPerPage] = React.useState(5);
+
+  const randomNumber = Math.floor(Math.random() * allData.length);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = allData.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div className="page__wrapper flex-col">
@@ -23,7 +35,7 @@ const App: FC = () => {
             <PositionSearchbar />
             <div className="bottom__container flex-row">
               <LocationSearchbar />
-              <Results />
+              <Results posts={currentPosts} />
             </div>
           </>
         )}
