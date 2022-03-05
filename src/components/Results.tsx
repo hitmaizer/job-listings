67cb0fcbe61183d48ad1e99df938f537047f7d37
hitmaizer <jose.sousalves@gmail.com>
2022-03-05 19:14:58 +0000

    props to card fixed

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,19 +1,52 @@
 import React from "react";
+import { DataObj } from "../interfaces/DataInterface";
 import Pagination from "./Pagination";
 import ResultCard from "./ResultCard";
 
-type Props = {};
+interface Props {
+  posts: {
+    id?: number;
+    url: string;
+    title: string;
+    company_name: string;
+    company_logo: string;
+    category: string;
+    tags: string[];
+    job_type: string;
+    publication_date: string;
+    candidate_required_location: string;
+    salary?: string;
+    description: string;
+    company_logo_url?: string;
+  }[];
+}
 
-function Results({}: Props) {
+const Results: React.FC<Props> = (props: Props) => {
+  const cardElements = props.posts.map((item) => {
+    return (
+      <ResultCard
+        key={item.id}
+        url={item.url}
+        title={item.title}
+        company_name={item.company_name}
+        company_logo={item.company_logo}
+        category={item.category}
+        tags={item.tags}
+        job_type={item.job_type}
+        publication_date={item.publication_date}
+        candidate_required_location={item.candidate_required_location}
+        salary={item.salary}
+        description={item.description}
+      />
+    );
+  });
+
   return (
     <div className="section__wrapper flex-col results">
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
-      <ResultCard />
+      {cardElements}
       <Pagination />
     </div>
   );
-}
+};
 
 export default Results;
