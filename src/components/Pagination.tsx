c5529af375aaad831ef35d823cf63a1c9e08fbec
hitmaizer <jose.sousalves@gmail.com>
2022-03-06 11:16:19 +0000

    mapping over pagenumbers + render pagination items

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,11 +8,22 @@ type Props = {
 };
 
 const Pagination: React.FC<Props> = (props: Props) => {
+  const pageNumbers: number[] = [];
+
+  for (let i = 1; i <= Math.ceil(props.totalPosts / props.postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const paginationElements = pageNumbers.map((item) => (
+    <StyledPagination children={item} />
+  ));
+
   return (
     <div className="pagination__wrapper flex-row">
       <StyledPagination>
         <NavigateBefore size="16px" />
       </StyledPagination>
+      {paginationElements}
       <StyledPagination>
         <NavigateNext size="16px" />
       </StyledPagination>
