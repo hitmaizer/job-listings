31775a654449f62468e2c1e94338fa33d7fa6546
hitmaizer <jose.sousalves@gmail.com>
2022-03-14 19:31:56 +0000

    added wrapper for styling

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -26,21 +26,23 @@ const ResultCard: FC<Props> = (props: Props) => {
       onClick={(id) => props.handleSpecificPost(props.id)}
     >
       <img src={props.company_logo} alt="" className="card__img" />
-      <div className="card__info flex-col">
-        <h6 className="info__company">{props.company_name}</h6>
-        <h1 className="info__title">{props.title}</h1>
-        {props.job_type === "full_time" && (
-          <h5 className="info__badge">Full time</h5>
-        )}
-      </div>
-      <div className="card__details flex-row">
-        <div className="detail__item flex-row">
-          <Globe size="16px" />
-          <p className="item__text">{props.candidate_required_location}</p>
+      <div className="card__infos flex-row">
+        <div className="card__info flex-col">
+          <h6 className="info__company">{props.company_name}</h6>
+          <h1 className="info__title">{props.title}</h1>
+          {props.job_type === "full_time" && (
+            <h5 className="info__badge">Full time</h5>
+          )}
         </div>
-        <div className="detail__item flex-row">
-          <Clock size="16px" />
-          <p className="item__text">{props.publication_date}</p>
+        <div className="card__details flex-row">
+          <div className="detail__item flex-row">
+            <Globe size="16px" />
+            <p className="item__text">{props.candidate_required_location}</p>
+          </div>
+          <div className="detail__item flex-row">
+            <Clock size="16px" />
+            <p className="item__text">{props.publication_date}</p>
+          </div>
         </div>
       </div>
     </StyledCard>
